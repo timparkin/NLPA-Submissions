67ce83a0eeb0a6e877097e0ac913b2ed4800ebc3
Tim Parkin <info@timparkin.co.uk>
2021-03-26 16:06:09 +0000

    changed db to postgres

--- nlpa/settings/base.py
+++ nlpa/settings/base.py
@@ -97,9 +97,14 @@ WSGI_APPLICATION = 'nlpa.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'nlpa',                      # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': 'nlpa',
+            'PASSWORD': 'nlpa',
+            'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'PORT': '',                      # Set to empty string for default.
     }
 }
 
