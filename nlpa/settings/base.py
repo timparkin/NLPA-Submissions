a47182c8d91c5cb580389882b5fb3d66f2803af7
Tim Parkin <info@timparkin.co.uk>
2021-03-31 10:36:59 +0100

    added external config for secure settings

--- nlpa/settings/base.py
+++ nlpa/settings/base.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os, config
+import os
+from .config import *
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
