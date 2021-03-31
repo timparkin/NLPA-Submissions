157fac03ad4bd8b3ffc81c98c01446b0bf17ce8d
Tim Parkin <info@timparkin.co.uk>
2021-03-31 19:05:15 +0100

    trying to configure payments

--- nlpa/settings/dev.py
+++ nlpa/settings/dev.py
@@ -12,6 +12,11 @@ ALLOWED_HOSTS = ['*']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
+STRIPE_PUBLISHABLE_KEY = 'pk_test_51IUFQmIyfIE0cGLT4tEHeLbn31247AnPgB2GZllvhbZAKZ0s0vUZsTjXn9EVR4XNdlXwQh1aTlvaXCM2anDxffeF008gTb6r3y'
+STRIPE_SECRET_KEY = 'sk_test_51IUFQmIyfIE0cGLTvNmGC7fWX3xuvnaTuXWu8Bndi7AhKvKjutDs5Fgb6CshG9jjJzLXAoOCE9Y4ZWHGm1mePW2s007sOLku4X'
+
+
+
 try:
     from .local import *
 except ImportError:
