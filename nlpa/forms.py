3350dc5d0551af41cb3956b8398eb2a09b786e96
Tim Parkin <info@timparkin.co.uk>
2021-04-17 11:39:34 +0100

    added a form to select the payment profile and pass to purchase - no data transfer to stripe yet

---
+++ nlpa/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+
+
+ENTRY_CHOICES = [('6', 'Six Entries'), ('12', 'Twelve Entries'), ('18', 'Eighteen Entries')]
+PORTFOLIO_CHOICES = [('0','No Porfolios'), ('1','One Porfolio'), ('2','Two Porfolios')]
+
+
+class PaymentPlanForm(forms.Form):
+    number_of_entries = forms.ChoiceField(widget=forms.Select, choices=ENTRY_CHOICES, label='Number of Entries')
+    number_of_portfolios = forms.ChoiceField(widget=forms.Select, choices=PORTFOLIO_CHOICES, label='Number of Portfolios')
--- payments/urls.py
+++ payments/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('paymentplanconfirm/', views.PaymentPlanConfirmView.as_view()),
     path('config/', views.stripe_config),
     path('create-checkout-session/', views.create_checkout_session),
     path('success/', views.SuccessView.as_view()),
