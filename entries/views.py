a25a59bb87e6288e8bc5b1ceaf148c7476fbebaf
Tim Parkin <info@timparkin.co.uk>
2021-04-29 21:11:21 +0100

    form shows the filename when uploaded with tooltips for long filenames etc

--- entries/views.py
+++ entries/views.py
@@ -13,20 +13,25 @@ from django.utils.safestring import mark_safe
 from entries.models import Entry
 from userauth.models import CustomUser as User
 
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
 
 
 categories=['Grand Landscape','Intimate and Abstract','Nightscape','Aerial']
 
-
 class ImageWidget(forms.widgets.ClearableFileInput):
     template_name = 'django/forms/widgets/clearable_file_input.html'
+
+
+
+
 
 @login_required
 def get_entries(request):
     user = request.user
     entries = Entry.objects.filter(user=user.id)
-    print(entries)
-    EntryInlineFormSet = inlineformset_factory(User, Entry, fields=('photo','category'), can_delete=True, max_num=6, min_num=6, widgets={'photo':ImageWidget})
+
+    EntryInlineFormSet = inlineformset_factory(User, Entry, fields=('photo','category','filename'), can_delete=True, max_num=6, min_num=6, widgets={'filename':forms.HiddenInput,'photo':ImageWidget})
 
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
@@ -35,6 +40,9 @@ def get_entries(request):
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
+            myformset = form.save(commit=False)
+            for f in myformset:
+                f.filename = f.photo.name
             form.save()
 
             # redirect to a new URL:
