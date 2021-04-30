60b6dcd0dca5c00382d4e7c482d9318a85f527a9
root <root@onlandscape2.dh.bytemark.co.uk>
2021-04-30 13:20:36 +0100

    fixed a few minor issues with deployment - upload filename was required, login/logout went missing

--- entries/models.py
+++ entries/models.py
@@ -15,7 +15,7 @@ class Entry(models.Model):
         aerial = 'A', _('Aerial')
 
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, null=True)
-    filename = models.CharField(verbose_name=_("Filename"), max_length=128, default='')
+    filename = models.CharField(verbose_name=_("Filename"), max_length=128, default='', blank=True, null=True)
     photo = ImageField(verbose_name=_("Entry Photo"), upload_to='entries/', default='entries/default-entry.png', pregenerated_sizes=["small", "large"])
     datetime = models.DateTimeField(verbose_name=_("Uploaded date"), auto_now_add=True, null=True)
     description = models.CharField(verbose_name=_("Description"), max_length=2048, blank=True, null=True)
--- nlpa/settings/base.py
+++ nlpa/settings/base.py
@@ -110,20 +110,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'nlpa.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'nlpa',                      # Or path to database file if using sqlite3.
-            # The following settings are not used with sqlite3:
-            'USER': 'nlpa',
-            'PASSWORD': 'nlpa',
-            'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-            'PORT': '',                      # Set to empty string for default.
-    }
-}
 
 # Auth User ModelBackend
 AUTH_USER_MODEL = 'userauth.CustomUser'
