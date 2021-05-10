46aa5fb3ea18c2f850369319b42054546e8fb678
Tim Parkin <info@timparkin.co.uk>
2021-05-10 21:38:25 +0100

    fixed the slowdown shite

--- entries/views.py
+++ entries/views.py
@@ -17,21 +17,43 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 import json
 from nlpa.custom_storages import create_custom_storage, CustomS3Boto3Storage
 
+def filesizeformat(bytes):
+    """
+    Formats the value like a 'human-readable' file size (i.e. 13 KB, 4.1 MB,
+    102 bytes, etc).
+    """
+    try:
+        bytes = float(bytes)
+    except (TypeError,ValueError,UnicodeDecodeError):
+        return u"0 bytes"
+
+    if bytes < 1024:
+        return ungettext("%(size)d byte", "%(size)d bytes", bytes) % {'size': bytes}
+    if bytes < 1024 * 1024:
+        return ugettext("%.1f KB") % (bytes / 1024)
+    if bytes < 1024 * 1024 * 1024:
+        return ugettext("%.1f MB") % (bytes / (1024 * 1024))
+    return ugettext("%.1f GB") % (bytes / (1024 * 1024 * 1024))
 
 
 category_list = ['GL', 'IA', 'N', 'A']
+
+entries_categories = (
+        ('GL','Grand Landscape'),
+        ('IA','Intimate & Abstract'),
+        ('N','Nightscape'),
+        ('A','Aerial'),
+
+)
 
 class ImageWidget(forms.widgets.ClearableFileInput):
     template_name = 'django/forms/widgets/clearable_file_input.html'
 
 
 
-
-
 @login_required
 def get_entries(request):
     payment_status = request.user.payment_status
-    print(payment_status)
     if payment_status is None or ('checkout.session.completed' not in payment_status and 'payment_pending' not in payment_status):
         return HttpResponseRedirect('/paymentplan')
     user = request.user
@@ -43,18 +65,36 @@ def get_entries(request):
 
 
 
-    EntryInlineFormSet = inlineformset_factory(User, Entry, fields=('photo','filename', 'category',), can_delete=False, max_num=int(payment_plan['entries']), min_num=int(payment_plan['entries']), widgets={'filename':forms.HiddenInput,'photo':ImageWidget})
+    EntryInlineFormSet = inlineformset_factory(User,
+                            Entry,
+                            fields=('photo','filename', 'category','photo_size','photo_dimensions'),
+                            can_delete=False,
+                            max_num=int(payment_plan['entries']),
+                            min_num=int(payment_plan['entries']),
+                            widgets={
+                                'filename':forms.HiddenInput,
+                                'photo_dimensions':forms.HiddenInput,
+                                'photo_size':forms.HiddenInput,
+                                'photo':ImageWidget,
+                                })
 
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = EntryInlineFormSet(request.POST, request.FILES, instance=user, queryset=Entry.objects.select_related('user').filter(category__in=category_list))
+        form = EntryInlineFormSet(request.POST, request.FILES, instance=user, queryset=Entry.objects.filter(category__in=category_list))
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
             myformset = form.save(commit=False)
             for f in myformset:
-                f.photo.storage.custom = {'filename': f.photo.name, 'user_email': request.user.email, 'category': f.category, 'is_young_entrant': request.user.is_young_entrant}
+                f.photo.storage.custom = {
+                                'filename': f.photo.name,
+                                'user_email': request.user.email,
+                                'category': f.category,
+                                'is_young_entrant': request.user.is_young_entrant
+                                }
+                f.photo_dimensions = '%s x %s'%(f.photo.width, f.photo.height)
+                f.photo_size = f.photo.size
                 f.filename = f.photo.name
             form.save()
 
@@ -63,7 +103,7 @@ def get_entries(request):
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = EntryInlineFormSet(instance=user, queryset=Entry.objects.select_related('user').filter(category__in=category_list))
+        form = EntryInlineFormSet(instance=user, queryset=Entry.objects.filter(category__in=category_list))
 
     return render(request, 'entries.html', {'formset': form})
 
@@ -77,10 +117,6 @@ def get_entries(request):
 
 class GetPortfolios(LoginRequiredMixin, View):
     template_name = 'portfolios.html'
-
-
-
-
 
     def get_context_data(self, **kwargs):
         if self.request.user.payment_plan is not None:
