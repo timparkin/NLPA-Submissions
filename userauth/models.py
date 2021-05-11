d335375ab370cde1e973a879e9d0fcb124e255f7
Tim Parkin <info@timparkin.co.uk>
2021-05-11 17:04:56 +0100

    added loads of amends for layout and messages and made entries the homepage

--- userauth/models.py
+++ userauth/models.py
@@ -37,4 +37,4 @@ class CustomUser(AbstractUser):
         return reverse('account_profile')
 
     def __str__(self):
-        return f"{self.username}: {self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name} ({self.email})"
