2e8c1ce9aa005d5d576712882c4d8f7aa89ba9ee
Tim Parkin <info@timparkin.co.uk>
2021-05-17 13:32:45 +0100

    added coupon creating code

---
+++ create-coupons.py
@@ -0,0 +1,30 @@
+import stripe
+import yaml
+stripe.api_key = '***REMOVED***'
+
+coupons = yaml.safe_load(open("coupons_live.yaml"))
+
+for coupon in coupons:
+
+    if 'code' not in coupon and 'name' in coupon:
+        coupon['id'] = '%s%s'%(coupon['name'].split(' ')[1].upper(),coupon['amount'])
+    else:
+        coupon['id'] = '%s%s'%(coupon['code'], coupon['amount'])
+
+    if 'name' not in coupon:
+        coupon['cname'] = coupon['id']
+    else:
+        coupon['cname'] = '%s (%s)'%(coupon['name'], coupon['id'])
+    print('Creating:')
+    print(coupon)
+    try:
+        response = stripe.Coupon.create(
+          percent_off=coupon['amount'],
+          duration="forever",
+          id=coupon['id'],
+          name=coupon['cname'],
+        )
+        print('Response:')
+        print(response)
+    except stripe.error.InvalidRequestError as e:
+        print(e)
