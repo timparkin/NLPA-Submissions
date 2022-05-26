5ec9794586da98833b3044e41b7cbb45b589a591
Tim Parkin <info@timparkin.co.uk>
2022-05-26 21:29:47 +0100

    added tooltips

--- entries/templatetags/filter_extras.py
+++ entries/templatetags/filter_extras.py
@@ -64,17 +64,19 @@ def decodepaymentplan(value):
 
 @register.filter
 def is_big_enough(value):
-    """check for 2048px is OK, 1600px is maybe and add icons"""
+    """check for 4000px is OK, 3000px is maybe and add icons"""
     if 'x' in value:
         wtext,htext = value.split(' x ')
         w = int(wtext)
         h = int(htext)
-        if w>2047 or h>2047:
+        if w>4000 or h>4000:
+            return mark_safe('<span data-bs-toggle="tooltip" data-bs-placement="top" title="Image must not be greater than 4000px on the long side"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#EE0000" class="bi bi-x-circle-fill" viewBox="0 0 16 16">  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/></svg></span>')
+        elif w==4000 or h==4000:
             return mark_safe('<span data-bs-toggle="tooltip" data-bs-placement="top" title="image size OK"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#008800" class="bi bi-check-circle-fill" viewBox="0 0 16 16">  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg></span>')
-        elif w>1600 or h>1600:
-            return mark_safe('<span data-bs-toggle="tooltip" data-bs-placement="top" title="we recommemd at least 2048px on the long side"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FF8800" class="bi bi-question-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/></svg></span>')
+        elif w>3000 or h>3000:
+            return mark_safe('<span data-bs-toggle="tooltip" data-bs-placement="top" title="3000px or greater is OK but we recommemd 4000px on the long side"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FF8800" class="bi bi-question-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/></svg></span>')
         else:
-            return mark_safe('<span data-bs-toggle="tooltip" data-bs-placement="top" title="Image must be greater than 1600px on the long side. We recommended 2048px"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#EE0000" class="bi bi-x-circle-fill" viewBox="0 0 16 16">  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/></svg></span>')
+            return mark_safe('<span data-bs-toggle="tooltip" data-bs-placement="top" title="Image must 3000px or greater on the long side. We recommended 4000px"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#EE0000" class="bi bi-x-circle-fill" viewBox="0 0 16 16">  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/></svg></span>')
 
     else:
         return mark_safe('<span data-bs-toggle="tooltip" data-bs-placement="top" title="There is a problem with this image, please try again"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#EE0000" class="bi bi-x-circle-fill" viewBox="0 0 16 16">  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/></svg></span>')
